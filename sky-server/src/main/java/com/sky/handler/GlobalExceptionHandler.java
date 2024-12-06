84eacba2bec50d62eaa3273e34dc9a7c859e5578
yezhian <123@123>
2024-12-06 14:41:50 +0800

    新增员工完成

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -5,6 +5,11 @@ import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import static com.sky.constant.MessageConstant.*;
+import java.sql.SQLIntegrityConstraintViolationException;
+
+
+
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +27,21 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        log.error("异常信息：{}", ex.getMessage());
+        String message=ex.getMessage();
+        if (message.contains("Duplicate entry")){
+            String [] str =message.split(" ");
+            String username=str[2];
+            String msg=username+ALREADY_EXIST;
+            return Result.error(msg);
+        }else{
+            String msg=UNKNOWN_ERROR;
+            return Result.error(msg);
+        }
     }
 
 }
--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,9 +1,13 @@
 package com.sky.service;
 
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 
 public interface EmployeeService {
+
+
+     void save(EmployeeDTO employeeDTO);
 
     /**
      * 员工登录
